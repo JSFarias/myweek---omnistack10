b6b5fec8c7f9f8a3f91d74f2f901057fec3de4b3
Jhonatas <jhonatas.s.farias@gmail.com>
2020-01-17 08:57:03 -0300

    connecting to mongodb atlas

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 
-app.get('/', (request, response) => {
-    return response.json({
-        message: "Hello JHONATAS"
+mongoose.connect('mongodb+srv://jhonatas:Jh0nnMong0@cluster0-p3mkb.mongodb.net/myweek10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(express.json())
+
+app.post('/', (request, response) => {
+    console.log(request.body)
+    return response.json({        
+        message: "Hello JHON"
     })
 })
 
