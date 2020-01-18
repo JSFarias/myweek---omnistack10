49aa76ef3d1fbfa48b66cf16b4fbbcb5b884659b
Jhonatas <jhonatas.s.farias@gmail.com>
2020-01-18 10:57:50 -0300

    mongodb atlas CRUD

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const routes = require('./routes')
+
+const app = express()
+
+
+mongoose.connect('mongodb+srv://jhonatas:Jh0nnMong0@cluster0-keazq.mongodb.net/all_devs?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+app.use(express.json())
+app.use(routes)
+
+
+app.listen(4545)
